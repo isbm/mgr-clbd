ce94c2e6a7ccf5f60ec03829292724cec358fe00
Bo Maryniuk <bo@suse.de>
2020-02-05 22:44:31 +0100

    Create a first app

--- cmd/mgr-clbd.go
+++ cmd/mgr-clbd.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/isbm/mgr-clbd"
+)
+
 func main() {
+	h := clbd.NewStatsHandler()
+
+	ep := clbd.NewAPIEndPoint()
+	ep.AddHandler("/ping", "post", h)
+	ep.Start()
 }
