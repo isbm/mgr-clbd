5d3367bd2476d112e253a512708c77ee33f3854c
Bo Maryniuk <bo@suse.de>
2020-02-07 19:30:12 +0100

    Add handlers in a better way, use config

--- clbd.go
+++ clbd.go
@@ -1,7 +1,10 @@
 package clbd
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"path"
+	"strings"
 )
 
 const (
@@ -12,28 +15,47 @@ const (
 
 type APIEndPoint struct {
 	server *gin.Engine
+	root   string
+	port   int
 }
 
-func NewAPIEndPoint() *APIEndPoint {
+func NewAPIEndPoint(root string) *APIEndPoint {
 	api := new(APIEndPoint)
+	api.root = "/" + strings.Trim(root, "/")
 	api.server = gin.Default()
+	api.port = 8080
 
 	return api
 }
 
-func (api *APIEndPoint) AddHandler(uri string, method string, handler Handler) {
-	switch method {
-	case GET:
-		api.server.GET(uri, handler.Handle)
-	case POST:
-		api.server.POST(uri, handler.Handle)
-	case ANY:
-		api.server.Any(uri, handler.Handle)
+func (api *APIEndPoint) SetPort(port int) *APIEndPoint {
+	api.port = port
+	return api
+}
+
+// getFullURN joins root and the given URN into a full path
+func (api *APIEndPoint) getFullURN(urn string) string {
+	return path.Join(api.root, strings.Trim(urn, "/"))
+}
+
+// Add handlers to the server
+func (api *APIEndPoint) AddHandler(handler Handler) *APIEndPoint {
+	urn := api.getFullURN(handler.URN())
+	for _, method := range handler.Methods() {
+		switch method {
+		case GET:
+			api.server.GET(urn, handler.Handle)
+		case POST:
+			api.server.POST(urn, handler.Handle)
+		case ANY:
+			api.server.Any(urn, handler.Handle)
+		}
 	}
+	return api
 }
 
 func (api *APIEndPoint) Start() {
-	err := api.server.Run(":9090")
+	err := api.server.Run(fmt.Sprintf(":%d", api.port))
 	if err != nil {
 		panic(err)
 	}
