ac1a992a9ef37ec81e7627424df0778a9e78a80a
Bo Maryniuk <bo@suse.de>
2020-02-07 17:10:54 +0100

    Rename handler, implement missing methods

---
+++ pinghandler.go
@@ -0,0 +1,37 @@
+package clbd
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+type PingHandler struct {
+	urn     string
+	methods []string
+}
+
+func NewPingHandler() *PingHandler {
+	ph := new(PingHandler)
+	ph.urn = "ping"
+	ph.methods = []string{"POST"}
+
+	return ph
+}
+
+// URN returns uniform resource name of the handler to be installed at.
+func (ph *PingHandler) URN() string {
+	return ph.urn
+}
+
+// Methods returns available methods that this handler supposed to handle
+func (ph *PingHandler) Methods() []string {
+	return ph.methods
+}
+
+// Handle implements the entry point of the handler
+func (ph *PingHandler) Handle(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"time": time.Now().Format(time.RFC1123Z),
+	})
+}
