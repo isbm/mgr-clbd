5ee5f89f1d7309424e2c96a1e9eca4fffc11f9ec
Bo Maryniuk <bo@suse.de>
2020-02-07 19:30:30 +0100

    Use config

--- cmd/mgr-clbd.go
+++ cmd/mgr-clbd.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/isbm/go-nanoconf"
 	"github.com/isbm/mgr-clbd"
 )
 
 func main() {
-	h := clbd.NewPingHandler()
-
-	ep := clbd.NewAPIEndPoint()
-	ep.AddHandler("/ping", "post", h)
+	cfg := nanoconf.NewConfig("./mgr-clbd.conf")
+	ep := clbd.NewAPIEndPoint("/api/v1").
+		SetPort(cfg.Find("api").Int("port", "")).
+		AddHandler(clbd.NewPingHandler())
 	ep.Start()
 }
