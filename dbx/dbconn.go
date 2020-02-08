32da27aa22b21a6239aa45d906714763f91e666f
Bo Maryniuk <bo@suse.de>
2020-02-08 19:11:46 +0100

    Avoid nameclash

--- dbx/dbconn.go
+++ dbx/dbconn.go
@@ -2,6 +2,7 @@ package dbx
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
@@ -16,69 +17,69 @@ type Dbx struct {
 
 // NewDbx is a constructor for database connection class
 func NewDbxConnection() *Dbx {
-	dbx := new(Dbx)
-	dbx._host = "localhost"
-	dbx._port = 3306
-	return dbx
+	d := new(Dbx)
+	d._host = "localhost"
+	d._port = 3306
+	return d
 }
 
 // SetUser sets the username for the database connection credentials
-func (dbx *Dbx) SetUser(user string) *Dbx {
-	dbx._user = user
-	return dbx
+func (d *Dbx) SetUser(user string) *Dbx {
+	d._user = user
+	return d
 }
 
 // SetPassword sets the password credentials for the database connection
-func (dbx *Dbx) SetPassword(password string) *Dbx {
-	dbx._password = password
-	return dbx
+func (d *Dbx) SetPassword(password string) *Dbx {
+	d._password = password
+	return d
 }
 
 // SetDBHost sets the hostname of the remote database URI
-func (dbx *Dbx) SetDBHost(fqdn string) *Dbx {
-	dbx._host = fqdn
-	return dbx
+func (d *Dbx) SetDBHost(fqdn string) *Dbx {
+	d._host = fqdn
+	return d
 }
 
 // SetPort sets the connection port of the remote database URI
-func (dbx *Dbx) SetDBPort(port int) *Dbx {
-	dbx._port = port
-	return dbx
+func (d *Dbx) SetDBPort(port int) *Dbx {
+	d._port = port
+	return d
 }
 
 // SetDBName sets the name of the database
-func (dbx *Dbx) SetDBName(dbname string) *Dbx {
-	dbx._dbname = dbname
-	return dbx
+func (d *Dbx) SetDBName(dbname string) *Dbx {
+	d._dbname = dbname
+	return d
 }
 
 // Close function closes the database connection
-func (dbx *Dbx) Close() error {
+func (d *Dbx) Close() error {
 	var err error
-	if dbx.db != nil {
-		err = dbx.db.Close()
+	if d.db != nil {
+		err = d.db.Close()
 	}
 	return err
 }
 
 // Open function sets the whole connection to the database opened state
-func (dbx *Dbx) Open() error {
+func (d *Dbx) Open() error {
 	var err error
-	uri := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", dbx._user, dbx._password, dbx._host, dbx._port, dbx._dbname)
-	dbx.db, err = gorm.Open("mysql", uri)
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", d._user, d._password, d._host, d._port, d._dbname)
+	d.db, err = gorm.Open("mysql", uri)
 	if err == nil {
-		dbx.db.DB().SetMaxIdleConns(50)
+		d.db.DB().SetMaxIdleConns(50)
 	}
 	return err
 }
 
 // DB returns the database connection. If database is not opened yet, it will automatically open a new connection.
-func (dbx *Dbx) DB() *gorm.DB {
-	if dbx.db == nil {
-		err := dbx.Open()
+func (d *Dbx) DB() *gorm.DB {
+	if d.db == nil {
+		err := d.Open()
 		if err != nil {
 			panic(err.Error())
 		}
 	}
-	return dbx.db
+	return d.db
 }
