579d7e0838714b71ef5f2a003e89b618560d2754
Bo Maryniuk <bo@suse.de>
2020-02-10 15:41:29 +0100

    Update base handler: get validators and get logger

--- handlers/basehandler.go
+++ handlers/basehandler.go
@@ -1,12 +1,16 @@
 package hdl
 
 import (
+	"github.com/isbm/mgr-clbd/utils"
+	"github.com/sirupsen/logrus"
 	"path"
 	"strings"
 )
 
 type BaseHandler struct {
-	root string
+	root        string
+	_validators *utils.Validators
+	_logger     *logrus.Logger
 }
 
 // PrepareRoot is sanitising root string, turning into a root URI
@@ -21,4 +25,20 @@ func (bh BaseHandler) ToRoute(route string) string {
 
 func (bh *BaseHandler) URI() string {
 	return bh.root
+}
+
+// GetLogger returns initalised or an instance of working logger
+func (bh *BaseHandler) GetLogger() *logrus.Logger {
+	if bh._logger == nil {
+		bh._logger = utils.GetTextLogger(logrus.DebugLevel, nil)
+	}
+	return bh._logger
+}
+
+// GetValidators returns initialised or an instance of working validators
+func (bh *BaseHandler) GetValidators() *utils.Validators {
+	if bh._validators == nil {
+		bh._validators = utils.NewValidators()
+	}
+	return bh._validators
 }
