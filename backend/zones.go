1d146ada2bae7a8e21c8ce0f506a3a1fb9a660eb
Bo Maryniuk <bo@suse.de>
2020-02-10 15:41:46 +0100

    Implement CreateZone backend function

--- backend/zones.go
+++ backend/zones.go
@@ -1,5 +1,9 @@
 package backend
 
+import (
+	"fmt"
+)
+
 type Zones struct {
 	BaseBackend
 }
@@ -10,4 +14,35 @@ func NewZonesBackend() *Zones {
 
 func (z *Zones) StartUp() {
 	z.VerifyTables(&ClusterZone{})
+}
+
+// CreateZone creates a new zone
+func (z *Zones) CreateZone(name string, descr string) error {
+	var err error
+	zone := &ClusterZone{
+		Name: name, Description: descr,
+	}
+	if z.db.DB().NewRecord(zone) {
+		z.db.DB().Create(&zone)
+		logger.Infof("Added a new Zone %s", name)
+	} else {
+		err = fmt.Errorf("Unable to add a new Zone '%s'", name)
+		logger.Errorln(err.Error())
+	}
+
+	return err
+}
+
+// RemoveZone removes an empty zone. If zone still contains nodes,
+// it won't be removed, but error will be issued.
+func (z *Zones) RemoveZone(name string) error {
+	var err error
+
+	return err
+}
+
+// NodesInZone returns an amount of attached cluster nodes to the zone
+func (z *Zones) NodesInZone(name string) int {
+
+	return 0
 }
